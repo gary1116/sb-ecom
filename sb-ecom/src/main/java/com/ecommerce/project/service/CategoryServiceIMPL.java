cd610faeb7421c5fa300b082a7ef40f2eac4a9fd
gary1116 <garygodwin234@gmail.com>
2025-02-04 08:57:47 +0530

    added repository and did changes in serviceIMPL

--- sb-ecom/src/main/java/com/ecommerce/project/service/CategoryServiceIMPL.java
+++ sb-ecom/src/main/java/com/ecommerce/project/service/CategoryServiceIMPL.java
@@ -1,6 +1,8 @@
 package com.ecommerce.project.service;
 
 import com.ecommerce.project.model.Category;
+import com.ecommerce.project.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -12,30 +14,28 @@ import java.util.Optional;
 @Service
 public class CategoryServiceIMPL implements CategoryService{
 
-    private List<Category> categories =new ArrayList<>();
-    private Long nextId=1L;
+//    private List<Category> categories =new ArrayList<>();
+
+    @Autowired
+    private CategoryRepository categoryRepository;
 
 
     @Override
     public List<Category> getAllCategories() {
-        return categories;
+        return categoryRepository.findAll();
     }
 
     @Override
     public void createCategory(Category category) {
-        category.setCategoryId(nextId++);
-        categories.add(category);
-
+        categoryRepository.save(category);
     }
 
     @Override
     public String deleteCategory(Long categoryId) {
-        Category category= categories.stream()
-                .filter(c-> c.getCategoryId().equals(categoryId))
-                .findFirst()
-                .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND,"resource not found"));
+    Category deleteCategory = categoryRepository.findById(categoryId)
+        .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND,"Resource Not Found"));
 
-        categories.remove(category);
+    categoryRepository.delete(deleteCategory);
 
         return "Category deleted";
     }
@@ -43,17 +43,12 @@ public class CategoryServiceIMPL implements CategoryService{
     @Override
     public Category updateCategory(Category category, Long categoryId) {
 
-     Optional<Category> optionalCategory =   categories.stream()
-                .filter(c->c.getCategoryId().equals(categoryId))
-                .findFirst();
+        Category savedCategory = categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource Not Found"));
 
-     if (optionalCategory.isPresent()){
-         Category existingCategory=optionalCategory.get();
-         existingCategory.setCategoryName(category.getCategoryName());
-         return existingCategory;
-     }else{
-         throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Resource not found");
-     }
+        category.setCategoryId(categoryId);
+        savedCategory=categoryRepository.save(category);
+        return savedCategory;
     }
 
 
