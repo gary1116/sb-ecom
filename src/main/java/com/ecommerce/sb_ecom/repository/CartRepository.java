88a8fe99ca0e3f986c28f1459c1bfb6982761f41
gary1116 <garygodwin234@gmail.com>
2026-02-08 17:31:35 +0530

    added/updated Repositories

--- src/main/java/com/ecommerce/sb_ecom/repository/CartRepository.java
+++ src/main/java/com/ecommerce/sb_ecom/repository/CartRepository.java
@@ -4,10 +4,11 @@ import com.ecommerce.sb_ecom.model.Cart;
 import com.ecommerce.sb_ecom.model.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import java.util.List;
 
+@Repository
 public interface CartRepository extends JpaRepository<Cart,Long> {
 
     @Query("SELECT c from Cart c WHERE c.user.email=?1")
